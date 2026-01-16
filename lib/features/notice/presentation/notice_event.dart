892b8fc22f96cfba602104180e042d0f10b5f2ac
rojanparajuli <rojanparajuli13@gmail.com>
2026-01-16 17:57:51 +0545

    changes

---
+++ lib/features/notice/presentation/notice_event.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+abstract class NoticeEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchNoticeList extends NoticeEvent {
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/features/notice/presentation/notice_state.dart
@@ -0,0 +1,66 @@
+import 'package:equatable/equatable.dart';
+import 'package:gaaubesi_vendor/features/notice/domain/entity/notice_list_entity.dart';
+
+abstract class NoticeState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class NoticeListInitial extends NoticeState {}
+
+class NoticeListLoading extends NoticeState {}
+
+class NoticeListLoaded extends NoticeState {
+  final NoticeListResponse noticeListResponse;
+
+  NoticeListLoaded({required this.noticeListResponse});
+  @override
+  List<Object?> get props => [noticeListResponse];
+}
+
+class NoticeListError extends NoticeState {
+  final String message;
+
+  NoticeListError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class NoticeListPaginating extends NoticeState {}
+
+class NoticeListPaginated extends NoticeState {
+  final NoticeListResponse noticeListResponse;
+
+  NoticeListPaginated({required this.noticeListResponse});
+  @override
+  List<Object?> get props => [noticeListResponse];
+}
+
+class NoticeListPaginationError extends NoticeState {
+  final String message;
+
+  NoticeListPaginationError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class NoticeListSearching extends NoticeState {}
+
+class NoticeListSearchLoaded extends NoticeState {
+  final NoticeListResponse noticeListResponse;
+
+  NoticeListSearchLoaded({required this.noticeListResponse});
+  @override
+  List<Object?> get props => [noticeListResponse];
+}
+
+class NoticeListSearchError extends NoticeState {
+  final String message;
+
+  NoticeListSearchError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+} 
